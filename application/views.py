2e24f9d50be50c525611e881d4cf418cab1b4b8a
Ihor Pysmennyi <pysmennyi.ihor@lll.kpi.ua>
2024-01-14 20:16:54 +0200

    Rename readme

--- application/views.py
+++ application/views.py
@@ -1,5 +1,22 @@
 from application import app
+from flask import request
+from flask import jsonify
+
+import uuid
 
 @app.route('/hello')
 def home():
     return "Hello, World!"
+
+@app.route('/messages', methods=['POST'])
+def example():
+    print("new message received")
+    task = init_task(request.get_json())
+    return jsonify(task), 201
+
+def init_task(data):
+    task = {};
+    task["message"] = data["message"]
+    task["id"] = str(uuid.uuid4())
+    return task
+
